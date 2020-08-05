0d8cb0fddf0cf65c8b4dee84e8a2b9bb03ab3c8a
Francisco Giraldo <fgiraldo@wearenolte.com>
2020-08-05 13:43:35 -0500

    Added corresponding hooks

--- theme-setup.php
+++ theme-setup.php
@@ -16,37 +16,47 @@ $view_engine = new Renderer(
 /**
  * Config theme.
  */
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'html5' );
-add_theme_support( 'align-wide' );
-add_theme_support( 'responsive-embeds' );
-
-/**
- * Enqueue CSS & JS.
- */
-wp_enqueue_style(
-	'main-css',
-	get_template_directory_uri() . '/frontend/dist/bundle.css',
-	false,
-	'1.0'
-);
-
-wp_enqueue_script(
-	'script',
-	get_template_directory_uri() . '/frontend/dist/bundle.js',
-	false,
-	1.0,
-	true
+add_action(
+	'after_setup_theme',
+	function() {
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'html5' );
+		add_theme_support( 'align-wide' );
+		add_theme_support( 'responsive-embeds' );
+	}
 );
 
 /**
- * Remove unnecessary things.
+ * Enqueue CSS & JS. Remove jquery from the frontend only.
  */
-if ( ! is_admin() ) {
-	wp_deregister_script( 'jquery' );
-}
+add_action(
+	'wp_enqueue_scripts',
+	function() {
+		wp_enqueue_style(
+			'main-css',
+			get_template_directory_uri() . '/frontend/dist/bundle.css',
+			false,
+			'1.0'
+		);
 
+		wp_enqueue_script(
+			'script',
+			get_template_directory_uri() . '/frontend/dist/bundle.js',
+			false,
+			1.0,
+			true
+		);
+
+		if ( ! is_admin() ) {
+			wp_deregister_script( 'jquery' );
+		}
+	}
+);
+
+/**
+ * Remove unnecessary things in <head>.
+ */
 remove_action( 'wp_head', 'rsd_link' );
 remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'wp_shortlink_wp_head' );
